fe77c6845b5ce68ec2c64c5efa7f9afdca31f548
Juliendy Mendes <juliendy0708@gmail.com>
2024-06-02 16:49:32 -0400

    feat: adiciona CORS

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,21 @@
 import express from 'express';
+import cors from 'cors';
 
 const routes = require('./routes');
 const accountRoutes = require('./routes/accountRoute');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+// Configurações avançadas de CORS
+const corsOptions = {
+    origin: '*', // Substitua pelo domínio permitido
+    methods: ['GET', 'POST', 'PUT', 'DELETE'], // Métodos HTTP permitidos
+    allowedHeaders: ['Content-Type', 'Authorization'], // Cabeçalhos permitidos
+  };
+
+app.use(cors());
+
 app.use(express.json());
 app.use('/api', routes);
 
