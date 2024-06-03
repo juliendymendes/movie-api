2450448f98bc00af1e504eedf0d4934128c71ab2
Juliendy Mendes <juliendy0708@gmail.com>
2024-06-02 22:13:03 -0400

    feat: busca filmes por categoria

--- src/controllers/movieController.ts
+++ src/controllers/movieController.ts
@@ -19,3 +19,21 @@ exports.discoverMovies = async (req: Request, res: Response) => {
       .then((json: any) => res.send(json))
       .catch((err: string) => console.error('error:' + err));
 }
+
+// get filmes by category
+exports.getMoviesByCategory = async (req: Request, res: Response) => {
+    const { category } = req.params;
+    const url = `https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc&with_genres=${category}`;
+    const options = {
+      method: 'GET',
+      headers: {
+        accept: 'application/json',
+        Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI5ZDI0Nzg5ZWZhYmNhNGM0NjNmYTAyMzRhNmNhMzRmZiIsInN1YiI6IjYyOGE0YzE0ZjEwYTFhNzVjN2IzMjdkMCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.Xg3U6g6iB80XF21CO9EIbnszGPjIpGKlU4fAnBjsrgo'
+      }
+    };
+    
+    fetch(url, options)
+      .then((res: { json: () => any; }) => res.json())
+      .then((json: any) => res.send(json))
+      .catch((err: string) => console.error('error:' + err));
+}
--- src/routes/movieRoute.ts
+++ src/routes/movieRoute.ts
@@ -3,6 +3,10 @@ import express from "express";
 const router = express.Router();
 const movieController = require('../controllers/movieController');
 
+// get filmes by category
+router.get('/category/:category', movieController.getMoviesByCategory);
+
 router.get('/discover', movieController.discoverMovies);
+
 
 export default router;
